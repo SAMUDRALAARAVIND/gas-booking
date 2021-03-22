93ab681095e9c8bac02eff75dd61887096f4ed2f
premsai2030 <66376642+premsai2030@users.noreply.github.com>
2021-03-22 12:37:15 +0530

    Included pincode api

--- dbch.php
+++ dbch.php
@@ -1,5 +1,6 @@
 <?php
 
-$username = $_POST["username"] ;
-echo $username ;
+$pin_code = $_POST["pin_code"] ;
+$check = file_get_contents("https://api.postalpincode.in/pincode/".$pin_code) ;
+print_r($check) ;
 ?>
--- pincode.js
+++ pincode.js
@@ -1,13 +1,24 @@
 $(document).ready(() => 
 {
-    $("button").click(() =>
+    $("#pincode").blur(() =>
     {
         $.ajax({
-            url:"dbch.php" ,
-            type:"POST",
-            sucess:() => 
+            url: 'dbch.php' ,
+            type: 'POST' ,
+            data:{pin_code:$("#pincode").val()},
+            success:(data) => 
             {
-                alert("ok bro") ;
+               const pincode =JSON.parse(data)[0]["Status"] ;
+               if(pincode == "Success")
+               {
+                   $("#pincode_label").css("color","green") ;
+                   $("#pincode_label").text("Valid Pincode") ;
+               }
+               else
+               {
+                $("#pincode_label").css("color","red") ;
+                $("#pincode_label").text("Invalid Pincode") ;
+               }
             }
         }) ;
         
